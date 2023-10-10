07707d7b41fa46fcfba94da6a51fd2703469a6b9
dinh quoc viet <viet.dinh@aba.com.vn>
2023-10-10 15:53:09 +0700

    Initial project ReactJS with TypeScript

---
+++ src/Header.tsx
@@ -0,0 +1,12 @@
+import { UserIcon } from "./UserIcon";
+
+export const Header = () => (
+  <div>
+    <a href="./">Q & A</a>
+    <input type="text" placeholder="Search..." />
+    <a href="./signin">
+      <UserIcon />
+      <span>Sign In</span>
+    </a>
+  </div>
+);
---
+++ src/UserIcon.tsx
@@ -0,0 +1,4 @@
+import user from './user.svg';
+export const UserIcon = () =>(
+    <img src={user} alt="User icon" width="12px" />
+);
